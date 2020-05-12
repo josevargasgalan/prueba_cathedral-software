a4bb49e35ac619a003a870abc9eef634106e69fc
Jose <Jose.Vargas@payever.org>
2020-05-12 20:13:01 +0200

    ADD follow buttons to profile-header component

--- src/pages/user-page/components/profile/components/profile-header/profile-header.component.jsx
+++ src/pages/user-page/components/profile/components/profile-header/profile-header.component.jsx
@@ -6,10 +6,33 @@ function ProfileHeaderComponent() {
         <div className={classes['profile-header-wrapper']}>
             <div className={classes.logo}>
             </div>
+            <div className={classes['follow-buttons-wrapper']}>
             <div className={classes['follow-buttons']}>
-                <button>Te gusta</button>
-                <button>Siguiendo</button>
-                <button>Compartir</button>
+                <button>            
+                    <div className={classes.icon}>
+                        <img src={process.env.PUBLIC_URL + '/like.svg'} alt="logo"/>
+                    </div>
+                    <span>Te gusta</span>
+                </button>
+                <button>            
+                    <div className={classes.icon}>
+                        <img src={process.env.PUBLIC_URL + '/following.svg'} alt="logo"/>
+                    </div>
+                    <span>Siguiendo</span>
+                </button>
+                <button>            
+                    <div className={classes.icon}>
+                        <img src={process.env.PUBLIC_URL + '/share.svg'} alt="logo"/>
+                    </div>
+                    <span>Compartir</span>
+               </button>
+            </div>
+            <div className={classes['send-message-button']}>
+                <span>Enviar mensaje</span>
+                <div className={classes.icon}>
+                    <img src={process.env.PUBLIC_URL + '/pen.svg'} alt="logo"/>
+                </div>
+                </div>
             </div>
         </div>
     )
